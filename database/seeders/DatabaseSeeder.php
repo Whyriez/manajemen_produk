2f6000521ebff01c4b76efed1e8645a200b2cb1a
Alim Suma <sumaalim@gmail.com>
2025-04-25 19:35:22 +0800

    update table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Artisan;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        Artisan::call('db:seed', array('--class' => 'AkunSeeder'));
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
